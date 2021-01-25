b0ade6f419c0c0e9b176cf5679b5975dea497f78
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-01-25 17:30:27 +0100

    change to evans code

--- smart_bot/models/models.py
+++ smart_bot/models/models.py
@@ -2,17 +2,17 @@
 
 from odoo import models, fields, api, _
 
-
 class SmartBot(models.AbstractModel):
     # _name = 'smart_bot.smart_bot'
     _inherit = 'mail.bot'
     # _description = 'smart_bot.smart_bot'
 
     def _get_answer(self, record, body, values, command=False):
-        
+        print(record, body, values, command)
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
             # main flow
             if "hello bot" in body:
                 return _("Hello from Ehio Technologies!")
+                
         return super(SmartBot, self)._get_answer(record, body, values, command=False)
