043b0581cd46caef2b6c70c90ff1c2daab332874
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-01-25 17:40:17 +0100

    test again

--- smart_bot/models/models.py
+++ smart_bot/models/models.py
@@ -12,7 +12,7 @@ class SmartBot(models.AbstractModel):
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
             # main flow
-            if "hello bot" in body:
-                return "Hello from Ehio Technologies!"
+            if _("hello bot") in body or "hello bot" in body:
+                return _("Hello from Ehio Technologies!")
                 
         return super(SmartBot, self)._get_answer(record, body, values, command=False)
