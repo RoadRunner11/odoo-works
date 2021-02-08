fb7257146051635e76bce071007422c527b9e948
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-08 21:58:03 +0100

    add changes

--- smart_bot/models/models.py
+++ smart_bot/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, _, fields, api
-
+from scripts import utils
 class SmartBot(models.AbstractModel):
     # _name = 'smart_bot.smart_bot'
     _inherit = 'mail.bot'
@@ -12,7 +12,10 @@ class SmartBot(models.AbstractModel):
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
             # main flow
+            intent = utils.intent_ext(body)
             if _("hello bot") in body or "hello bot" in body:
                 return _("Hello from Ehio Technologies!")
-                
+            elif body == "createSale" or  body == "makeSale":
+                return _("Kindly provide the details of the sale order")
+   
         return super(SmartBot, self)._get_answer(record, body, values, command=False)
---
+++ smart_bot/scripts/rpc_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# coding: utf-8
+
+import xmlrpc.client as xc
+
+# import xlrd
+
+dbname = 'odoo13'
+
+username = 'admin'
+
+pwd = 'admin'
+
+sock_common = xc.ServerProxy ('http://18.216.227.139:8069/xmlrpc/2/common')
+
+sock = xc.ServerProxy('http://18.216.227.139:8069/xmlrpc/object')
+
+uid = sock_common.authenticate(dbname, username, pwd, {})
+
+def create_order():
+    account_ids = sock.execute(dbname, uid, pwd, 'sale.order', 'create', 
+                    {   'name': 'fist_order',
+                        'partner_id': 1, 'pricelist_id':1
+                        })
+
+    return "Sale Order Created Successfully...!"
