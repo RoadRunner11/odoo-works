bd1108633df71b8b55a3b707944fd4e5578343c5
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-09 00:24:04 +0100

    test again

--- smart_bot/models/models.py
+++ smart_bot/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, _, fields, api
-from scripts import utils
+from scripts.utils import intent_ext
 class SmartBot(models.AbstractModel):
     # _name = 'smart_bot.smart_bot'
     _inherit = 'mail.bot'
@@ -12,7 +12,7 @@ class SmartBot(models.AbstractModel):
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
             # main flow
-            intent = utils.intent_ext(body)
+            intent = intent_ext(body)
             if _("hello bot") in body or "hello bot" in body:
                 return _("Hello from Ehio Technologies!")
             elif body == "createSale" or  body == "makeSale":
