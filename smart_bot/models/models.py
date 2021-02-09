24d748011b1558503f92e61aabb32cfe5c630ed0
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-09 11:09:23 +0100

    tests it

--- smart_bot/models/models.py
+++ smart_bot/models/models.py
@@ -2,17 +2,20 @@
 
 from odoo import models, _, fields, api
 from .utils import intent_ext
+import logging
+
+_logger = logging.getLogger(__name__)
 class SmartBot(models.AbstractModel):
     # _name = 'smart_bot.smart_bot'
     _inherit = 'mail.bot'
     # _description = 'smart_bot.smart_bot'
 
     def _get_answer(self, record, body, values, command=False):
-        print(record, body, values, command)
+        _logger.debug(record, body, values)
         odoobot_state = self.env.user.odoobot_state
         if self._is_bot_in_private_channel(record):
             # main flow
-            intent = intent_ext(body)
+            # intent = intent_ext(body)
             if _("hello bot") in body or "hello bot" in body:
                 return _("Hello from Ehio Technologies!")
             elif body == "createSale" or  body == "makeSale":
