b04e939e7c7e50d05fd9644038b0827879d4b78f
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-16 11:36:04 +0100

    add first

--- payment_yoco/models/payment.py
+++ payment_yoco/models/payment.py
@@ -17,18 +17,18 @@ _logger = logging.getLogger(__name__)
 class PaymentAcquirerYoco(models.Model):
     _inherit = 'payment.acquirer'
 
-    provider = fields.Selection(selection_add=[
-        ('yoco', 'Yoco')
-    ], ondelete={'yoco': 'set default'})
-    yoco_merchant_id = fields.Char(string="Yoco Merchant ID", required_if_provider='yoco', groups='base.group_user')
-    yoco_account_id = fields.Char(string="Yoco Account ID", required_if_provider='yoco', groups='base.group_user')
-    yoco_api_key = fields.Char(string="Yoco API Key", required_if_provider='yoco', groups='base.group_user')
+#     provider = fields.Selection(selection_add=[
+#         ('yoco', 'Yoco')
+#     ], ondelete={'yoco': 'set default'})
+#     yoco_merchant_id = fields.Char(string="Yoco Merchant ID", required_if_provider='yoco', groups='base.group_user')
+#     yoco_account_id = fields.Char(string="Yoco Account ID", required_if_provider='yoco', groups='base.group_user')
+#     yoco_api_key = fields.Char(string="Yoco API Key", required_if_provider='yoco', groups='base.group_user')
 
-    def _get_yoco_urls(self, environment):
-        """ PayUlatam URLs"""
-        if environment == 'prod':
-            return 'https://online.yoco.com/v1/charges/'
-        return 'https://online.yoco.com/v1/charges/'
+#     def _get_yoco_urls(self, environment):
+#         """ PayUlatam URLs"""
+#         if environment == 'prod':
+#             return 'https://online.yoco.com/v1/charges/'
+#         return 'https://online.yoco.com/v1/charges/'
     
 
 class PaymentTransactionYoco(models.Model):
