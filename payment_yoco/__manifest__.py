3834989e9f97cce42f61fee5575ec78d063d9f23
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-16 11:17:27 +0100

    add changes

--- payment_yoco/__manifest__.py
+++ payment_yoco/__manifest__.py
@@ -12,7 +12,7 @@
     """,
 
     'author': "Fodilu Olawale Akorede",
-    'website': "https://www.ehiotech.com/
+    'website': "https://www.ehiotech.com/",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
