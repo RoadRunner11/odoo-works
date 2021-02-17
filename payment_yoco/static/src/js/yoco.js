4f1adb6a9a4dbb2e794dfb29ab3d5aa889c79bad
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-02-17 12:59:20 +0100

    rm else

--- payment_yoco/static/src/js/yoco.js
+++ payment_yoco/static/src/js/yoco.js
@@ -38,53 +38,54 @@ odoo.define('payment_stripe.stripe', function (require) {
     require('web.dom_ready');
     if (!$('.o_payment_form').length) {
         return Promise.reject("DOM doesn't contain '.o_payment_form'");
-    }else{
+    }
     
-        function display_yoco_form(provider_form){
-            // Open Checkout with further options
-            // if ($.blockUI) {
-            //     var msg = _t("Just one more second, We are redirecting you to Yoco...");
-            //     $.blockUI({
-            //         'message': '<h2 class="text-white"><img src="/web/static/src/img/spin.png" class="fa-pulse"/>' +
-            //                 '    <br />' + msg +
-            //                 '</h2>'
-            //     });
-            // }
-            var paymentForm = $('.o_payment_form');
-            if (!paymentForm.find('i').length) {
-                paymentForm.append('<i class="fa fa-spinner fa-spin"/>');
-                paymentForm.attr('disabled', 'disabled');
-            }
-
-            var _getYocoInputValue = function (name) {
-                return provider_form.find('input[name="' + name + '"]').val();
-            };
-
-            var yoco = new window.YocoSDK({
-                publicKey: _getYocoInputValue('yoco_pub_key'),
-              });
-            yoco.showPopup({
-                amountInCents: 2799,
-                currency: 'ZAR',
-                name: 'Your Store or Product',
-                description: 'Awesome description',
-                callback: function (result) {
-                    // This function returns a token that your server can use to capture a payment
-                    if (result.error) {
-                    const errorMessage = result.error.message;
-                    alert("error occured: " + errorMessage);
-                    } else {
-                    alert("card successfully tokenised: " + result.id);
-                    }
-                    // In a real integration - you would now pass this chargeToken back to your
-                    // server along with the order/basket that the customer has purchased.
-                }
-            })
+    function display_yoco_form(provider_form){
+        // Open Checkout with further options
+        if ($.blockUI) {
+            var msg = _t("Just one more second, We are redirecting you to Yoco...");
+            $.blockUI({
+                'message': '<h2 class="text-white"><img src="/web/static/src/img/spin.png" class="fa-pulse"/>' +
+                        '    <br />' + msg +
+                        '</h2>'
+            });
+        }
+        var paymentForm = $('.o_payment_form');
+        if (!paymentForm.find('i').length) {
+            paymentForm.append('<i class="fa fa-spinner fa-spin"/>');
+            paymentForm.attr('disabled', 'disabled');
         }
 
-        $.getScript("https://js.yoco.com/sdk/v1/yoco-sdk-web.js", function(data, textStatus, jqxhr) {
-            // observer.observe(document.body, {childList: true});
-            display_yoco_form($('form[provider="yoco"]'));
-        });
-    };
+        var _getYocoInputValue = function (name) {
+            return provider_form.find('input[name="' + name + '"]').val();
+        };
+
+        var yoco = new window.YocoSDK({
+            publicKey: _getYocoInputValue('yoco_pub_key'),
+            });
+
+        yoco.showPopup({
+            amountInCents: 2799,
+            currency: 'ZAR',
+            name: 'Your Store or Product',
+            description: 'Awesome description',
+            callback: function (result) {
+                // This function returns a token that your server can use to capture a payment
+                if (result.error) {
+                const errorMessage = result.error.message;
+                alert("error occured: " + errorMessage);
+                } else {
+                alert("card successfully tokenised: " + result.id);
+                }
+                // In a real integration - you would now pass this chargeToken back to your
+                // server along with the order/basket that the customer has purchased.
+            }
+        })
+    }
+
+    $.getScript("https://js.yoco.com/sdk/v1/yoco-sdk-web.js", function(data, textStatus, jqxhr) {
+        observer.observe(document.body, {childList: true});
+        display_yoco_form($('form[provider="yoco"]'));
+    });
+    
 })
